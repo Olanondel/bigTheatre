7362e75795884ce6090646ee88dc94f80dd656f8
Olanondel <prbaoleg@gmail.com>
2021-05-31 22:53:24 +0300

    22:53

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,25 @@ mobileMenuOpenBtn.addEventListener('click', (e) => {
 
     mainNav.classList.toggle('mobile-menu_active')
 })
+
+const productionSlider = new Swiper('.our-products__slider', {
+    slideToClickedSlide: true,
+    watchOverflow: true,
+    loop: true,
+    slidesPerView: 1,
+    speed: 800,
+    // autoplay: {
+    //   speed: 800
+    // },
+    slidesPerGroup: 1,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+
+    breakpoints: {
+        576: {
+            slidesPerView: 'auto',
+        }
+    }
+})
