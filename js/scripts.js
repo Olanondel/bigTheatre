97a8ba0f1af505be117e666449c38ecf6763f334
Olanondel <prbaoleg@gmail.com>
2021-06-02 02:14:40 +0300

    2:14

--- js/scripts.js
+++ js/scripts.js
@@ -37,19 +37,23 @@ const productionSlider = new Swiper('.our-products__slider', {
     }
 })
 
-const partnersSlider = new Swiper('.partners__row', {
+const partnersSliderElement = document.querySelector('.partners__row')
+
+let partnersSlider = new Swiper(partnersSliderElement, {
     slidesPerView: 1,
+    slidesPerGroup: 1,
     slideToClickedSlide: true,
     watchOverflow: true,
-    freeMode: true,
     grabCursor: true,
     slidesPerColumn: 2,
     breakpoints: {
         576: {
-            slidesPerView: 2
+            slidesPerView: 2,
+            freeMode: true,
         },
         768: {
             slidesPerView: 2.6,
+            freeMode: true,
         },
         1200: {
             slidesPerView: 4,
@@ -61,3 +65,55 @@ const partnersSlider = new Swiper('.partners__row', {
         onlyInViewport: true,
     },
 })
+
+window.addEventListener('resize', () => {
+
+    if (window.innerWidth > 1200 && partnersSliderElement.dataset.pc === 'false') {
+        partnersSliderElement.dataset.pc = 'true'
+
+    }
+
+    if (window.innerWidth < 1200 && partnersSliderElement.dataset.pc === 'true') {
+        partnersSlider = new Swiper(partnersSliderElement, {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            slideToClickedSlide: true,
+            watchOverflow: true,
+            grabCursor: true,
+            slidesPerColumn: 2,
+            breakpoints: {
+                576: {
+                    slidesPerView: 2,
+                    freeMode: true,
+                },
+                768: {
+                    slidesPerView: 2.6,
+                    freeMode: true,
+                },
+                1200: {
+                    slidesPerView: 4,
+                    enabled: false,
+                }
+            },
+            keyboard: {
+                enabled: true,
+                onlyInViewport: true,
+            },
+        })
+        partnersSliderElement.dataset.pc = 'false'
+    }
+})
+
+// add smooth anchors
+const anchors = document.querySelectorAll('a[href^="#"]')
+for (let anchor of anchors) {
+    anchor.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        const blockId = anchor.getAttribute('href')
+        document.querySelector(blockId).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
